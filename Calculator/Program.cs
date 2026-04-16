edbbb5634a8590bdf7c0424b866ba722a66dfaf5
sole <s.soriano@edu.gva.es>
2026-04-16 10:19:59 +0200

    Afig nou comentari

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Declare variables and then initialize to zero.
 int num1 = 0; int num2 = 0;
 
+//NOu comentari per a incloure en la documentació
 // Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("------------------------\n");
